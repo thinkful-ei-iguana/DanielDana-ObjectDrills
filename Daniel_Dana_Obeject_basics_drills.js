b006089c2635513b682e77841f1e830a29df3f93
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2019-10-23 08:57:11 -0700

    finished second drill object updater

--- Daniel_Dana_Obeject_basics_drills.js
+++ Daniel_Dana_Obeject_basics_drills.js
@@ -61,7 +61,11 @@ function createMyObject() {
 
 
 function updateObject(obj) {
-  // your code here
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+  return obj;
 }
   
   
