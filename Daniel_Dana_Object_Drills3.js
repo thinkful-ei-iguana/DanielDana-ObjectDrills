0a9c53fbce93ff2c5d3822a744b53c39fa96eb2b
Daniel Kent <Kentpercussion@gmail.com>
2019-10-23 12:45:45 -0600

    exercise 2 complete

--- Daniel_Dana_Object_Drills3.js
+++ Daniel_Dana_Object_Drills3.js
@@ -22,7 +22,17 @@ console.log(loaf.hydration());
 // Create an object with five properties: foo, bar, fum, quux, and spam. Give each property a unique value of your choosing.
 // Loop over the object using for ... in
 // Use console.log to show each property name and its associated value.
+const fiver = {
+  foo: 'foo',
+  bar: 'bar',
+  fum: 'fum',
+  quux: 'quux',
+  spam: 'spam'
+}
 
+for(let key in fiver) {
+  console.log(`${key} ${fiver[key]}`);
+}
 
 
 
