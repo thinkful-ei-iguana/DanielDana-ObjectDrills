35a9a4dee1aedb447c899abd5733857c4729b34c
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2019-10-23 13:33:05 -0700

    did a bunch of stuff in Rivendell. All is gewd

--- Daniel_Dana_Object_Drills3.js
+++ Daniel_Dana_Object_Drills3.js
@@ -148,17 +148,52 @@ console.log(decoder.decodeWords(arr56));
 
 // Each character should also have a method called evaluateFight that takes in a character object and returns the following string: "Your opponent takes {x} damage and you receive {y} damage" where x and y are the differences between each characters attack and defense values. If defense exceeds attack, then take zero damage.
 
+
+
+
 // Using array literal syntax, create an array characters that calls your factory function for each character in the table above with the relevant parameters. Your characters array should now have 5 objects in it.
 
 // Add a new character to characters (make up any attributes not provided):
 
+
+function createCharacter(name, nickname, race, origin, attack, defense) {
+  return {
+    name, nickname, race, origin, attack, defense,
+    describe() { console.log(`${this.name} is a ${this.race} from ${this.origin}`);},
+    evaluateFight(character) {
+      let opponentDamage = this.attack-character.defense;
+      let ownDamage = character.attack-this.defense;
+      return `Your opponent takes ${opponentDamage<0?0:opponentDamage} damage and you receive ${ownDamage<0? 0:ownDamage} damage`;
+
+    }
+
+  };
+}
+
+let characterArr = 'Gandalf the White         | gandalf     | Wizard     | Middle Earth   | 10       | 6        || Bilbo Baggins             | bilbo       | Hobbit     | The Shire      | 2        | 1        || Frodo Baggins             | frodo       | Hobbit     | The Shire      | 3        | 2       || Aragorn son of Arathorn   | aragorn     | Man        | Dunnedain      | 6        | 8'.split('||').map(x=>x.split(' | ')).map(y=>y.map(z=>z.trim()));
+let cast = characterArr.map(char=> createCharacter(...char));
+
+
+
 // Arwen Undomiel is a Half-Elf of Rivendell
+cast.push(createCharacter(
+'Arwen Undomiel', 
+'arwy',
+'Three Quarter-Elf of Rivendell vegan',
+'Rivendell',
+100,
+1000
+));
+
+console.log(cast);
 // Using the .find() function, retrieve your character nicknamed aragorn from characters and then call his describe method.
+cast.find(fighter=>fighter.nickname==='aragorn').describe();
 
 // Using the .filter() function, create a new array from characters that ONLY contains characters of the race Hobbit.
+console.log(cast.filter(person=>person.race==='Hobbit'));
 
 // Using the .filter() function, create a new array from characters that ONLY contains characters with attack value above 5.
-
+console.log(cast.filter(person=>person.attack>5));
 // What if you wanted to equip a weapon for each character and change how they are described? For example:
 
 // Gandolf the White is a Wizard of the Middle Earth who uses a wizard staff
@@ -168,4 +203,3 @@ console.log(decoder.decodeWords(arr56));
 // Legolas ... Bow and Arrow
 // Arwen .... Hadhafang
 // How would you change the factory function and other methods?
-
